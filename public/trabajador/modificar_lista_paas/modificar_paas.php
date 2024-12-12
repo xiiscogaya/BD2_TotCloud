b12b4fc7782c437181b865c79e0cd19521b4ce53
Ivan Spalla Ugalde <ivanuib332@gmail.com>
2024-12-12 12:09:24 +0100

    slider fixed

--- public/trabajador/modificar_lista_paas/modificar_paas.php
+++ public/trabajador/modificar_lista_paas/modificar_paas.php
@@ -157,7 +157,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['idPaaS'], $_POST['est
                                             name="estado" 
                                             value="Disponible" 
                                             onchange="this.form.submit()"
-                                            <?php echo $row['Estado'] !== 'En pruebas' ? 'checked' : ''; ?>
+                                            <?php echo $row['Estado'] === 'Disponible' ? 'checked' : ''; ?>
+                                            <?php echo $row['Estado'] === 'Activo' ? 'checked disabled' : ''; ?>
                                         >
                                     </label>
                                     <span class="slider-label">Disponible</span>
